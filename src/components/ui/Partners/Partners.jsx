5e5fcb98c11631d68b1e7ad0ad7df9617f40f9e9
fanwwi <farangiz.anvarjanova553@gmail.com>
2026-07-14 11:56:53 +0600

    .

--- src/components/ui/Partners/Partners.jsx
+++ src/components/ui/Partners/Partners.jsx
@@ -4,29 +4,73 @@ import styles from "./Partners.module.css";
 
 const partners = [
   {
-    name: "Harvard",
-    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSW6rsgkK17KedEwSEFJbkDpN5uN5v2EOquTgEPjdbvQq6GOIku-Gw1-hw&s=10",
+    name: "Arizona State University",
+    logo: "https://fundit.fr/sites/default/files/styles/max_650x650/public/actors/4105-universite-detat-larizona-asu.jpg?itok=fxkwwYmk",
   },
   {
-    name: "MIT",
-    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTEl9Rnn6T5WNnD7ZE8bSCikAiHzwdfP7MrNJHuRAH3CL28TF3gt172eqY&s=10",
+    name: "Sapienza University of Rome",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT41DkOD8jaYhHmU0wsNmkbi7Nz0fYsxCzt_CHVkEdZfvJ0266dvzfSUEOl&s=10",
   },
   {
-    name: "Stanford",
-    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSPXMKyqD3BQyoFlwzib8-mSdDBjxaz3f5WM_6LWBFmneAzH_-n_9xEn5Yl&s=10",
+    name: "DePaul University",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRt3G3EJjfnxUvZTq_fkdIMNe6ub5YlUyD5xeY5FVYr7jPgWfzkC3eLEJY&s=10",
   },
   {
-    name: "Oxford",
-    logo: "https://www.designyourway.net/blog/wp-content/uploads/2024/04/oxford-university-logo.jpg",
+    name: "Temple University",
+    logo: "https://graduatephiladelphia.org/wp-content/uploads/2016/01/Untitled-1_0001_Temple.jpg",
   },
   {
-    name: "Cambridge",
-    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFn61ebcN45AzUJmYSRJ1AHun-wxD4aP2-y0cJ7NKJvw&s=10",
+    name: "Rowan University",
+    logo: "https://logowik.com/content/uploads/images/rowan-university8573.jpg",
+  },
+  {
+    name: "Oregon State University",
+    logo: "https://pentagram-production.imgix.net/44374b63-afac-43a2-8b77-d161ccf25c6f/ds_osu_15_newwebsite.jpg?auto=compress%2Cformat&fit=min&fm=jpg&q=80&rect=%2C%2C%2C&w=640&s=4096a5970a75b8c028598c7a9ea85088",
+  },
+  {
+    name: "Colorado State University",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTMq6iEfwbF_33ALwvP8wMyiW_gGmXketeogH922SnFPw&s=10",
+  },
+  {
+    name: "Marshall University",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSIZll3ahjCfE0CIpM-JCkrXNQ6-ZViqaKjnTPnPA1KQW2S3-ojA3gUttA&s=10",
+  },
+  {
+    name: "George Mason University",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR8PFyIzwFXF5XIbr5VepHd4PM_zI7BWTVmBotSvteTukAuDhgGnhTLj9CO&s=10",
+  },
+  {
+    name: "DREW University",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR0jQ5brNyhmP96n_WnGBR7SoyTkHH6U6v6wBbOxGIv0Nc13huYrdXr5LI&s=10",
+  },
+  {
+    name: "Saint Louis University",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQqAwP31UJV3tIcFD-DVTQ30KY3_bd5Yrdzlgbyg069QLXwbciBGvrmN_E&s=10",
+  },
+  {
+    name: "The University of Alabama at Birmingham",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQWrwQNQ_Vjcurfuko-7oNurBHuHBwxDbv4uEqEl-4NAT_Fw-To1WAICWQj&s=10",
+  },
+  {
+    name: "Washington State University",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5ysAWq2AX2LKROqVhBa62ZKtJ3-AHMmnJgZ2a83csLac8dUcbJcEoTLsk&s=10",
+  },
+  {
+    name: "Suffolk University",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQnlmMcza6T9Pl-pnmnutcS6AxWG3zUzn8MnNCxDOgdwe_MuRr2L6BjzBaL&s=10",
+  },
+  {
+    name: "Hofstra University",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTdRC3F-o15AhzhNqHQEuBQccDX-qbqiTOTkrqs4731VZ21ZrX3WQw3YPo&s=10",
+  },
+  {
+    name: "Illinois State University",
+    logo: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTxfDlh-0T2iJqAWiT-w6Viuivw2K_koiYC4bdjPrEeBjRsMs4hr5pAtXY&s=10",
   },
 ];
 
 export default function Partners() {
-  const loop = [...partners, ...partners, ...partners];
+  const loop = [...partners, ...partners];
 
   return (
     <section className={styles.section}>
